19eb6a5eb49b08777f06e68f563ab59814171586
handsomenickname <qkrgusdk9@gmail.com>
2025-06-05 13:03:27 +0900

    스크립트 코드

--- project/Assets/Script/Script.cs
+++ project/Assets/Script/Script.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    int playerLevel = 10;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        name += "나의 이름은";
+        name += "김철수";
+        name += "입니다.";
+
+        Debug.Log(name);
+
+        Debug.Log("나의 레벨은" + playerLevel + "입니다.");
+        Debug.Log($"나의 레벨은 {playerLevel}입니다.");
+
     }
 }
