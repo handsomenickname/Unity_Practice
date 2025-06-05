03a988f53f38837879b2da6db6f2336dd8d78464
handsomenickname <qkrgusdk9@gmail.com>
2025-06-05 15:10:10 +0900

    스크립트 연습

--- project/Assets/Script/Script.cs
+++ project/Assets/Script/Script.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    int playerLevel = 10;
+    string Name = "박현아";
+    int age = 22;
+    int month = 12;
+    int date = 29;
+    int height = 162;
+    string mbti = "ISFJ";
+    string hobby = "밴드 연습";
+    string game = "오버워치";
+    string food = "삼겹살";
+    string major = "산업디자인";
+    string movie = "암살";
+
 
     private void Start()
     {
-        name += "나의 이름은";
-        name += "김철수";
-        name += "입니다.";
 
-        Debug.Log(name);
-
-        Debug.Log("나의 레벨은" + playerLevel + "입니다.");
-        Debug.Log($"나의 레벨은 {playerLevel}입니다.");
+        Debug.Log($"제 이름은 {Name}입니다.");
+        Debug.Log($"제 나이는 {age}입니다.");
+        Debug.Log($"제 생일은 {month}월 {date}일 입니다.");
+        Debug.Log($"제 키는 {height}입니다.");
+        Debug.Log($"제 MBTI는 {mbti}입니다.");
+        Debug.Log($"제 전공은 {major}입니다.");
+        Debug.Log($"제 취미는 {hobby}입니다.");
+        Debug.Log($"제가 좋아하는 게임은 {game}입니다.");
+        Debug.Log($"제가 좋아하는 음식은 {food}입니다.");
+        Debug.Log($"제가 좋아하는 영화는 {movie}입니다.");
 
     }
 }
